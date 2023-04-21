b6940b57e0ec7f14ce073cf91b01fe583e97e2d1
vinicius-hso <viniciushso@icloud.com>
2023-04-21 10:56:31 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<!-- * Composition API -->
+<script setup>
+import { ref } from "vue";
+
+// state
+const count = ref(0);
+
+const addToCount = () => {
+  count.value = count.value + 1;
+}
+
+const subtractFromCount = () => {
+  count.value = count.value - 1;
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is ....</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+/* scoped applies only to this style */
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div {
+  text-align: center;
+}
+</style>
